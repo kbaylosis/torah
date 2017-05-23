e6c81277f88cf64a3f17233df322cd1e6f56d449
Kenneth Baylosis <kbaylosis@zoogtech.com>
2017-05-23 08:38:50 +0800

    Add User api

---
+++ api/controllers/UserController.js
@@ -0,0 +1,14 @@
+/**
+ * UserController
+ *
+ * @description :: Server-side actions for handling incoming requests.
+ * @help        :: See https://sailsjs.com/docs/concepts/actions
+ */
+
+module.exports = {
+	find : (req, res) => {
+		setTimeout(() => {
+			res.ok();
+		}, 3000);
+	}
+};
